fbeb2b20d60b94d1b7e27786e164ef2f27b4a7d3
Feagly <paolowiiu@gmail.com>
2026-06-22 10:36:35 +0200

    feat: importa modelli User ed Event in db.py per la creazione tabelle

--- app/data/db.py
+++ app/data/db.py
@@ -5,6 +5,8 @@ import os
 from faker import Faker
 from app.config import config
 # TODO: remember to import all the DB models here
+from app.models.event import Event # NOQA
+from app.models.user import User # NOQA
 from app.models.registration import Registration  # NOQA
 
 
