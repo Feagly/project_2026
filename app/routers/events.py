b3fe52547973319f5692a332e82be588c54f6c87
Feagly <paolowiiu@gmail.com>
2026-06-22 12:00:28 +0200

    feat: aggiungi GET /events/{id}

--- app/routers/events.py
+++ app/routers/events.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Path
 from sqlmodel import select
 from app.models.event import Event, EventPublic
 from app.data.db import SessionDep
+from typing import Annotated
 
 router = APIRouter(prefix="/events")
 
@@ -10,3 +11,15 @@ router = APIRouter(prefix="/events")
 def get_all_events(session: SessionDep) -> list[EventPublic]:
     """Restituisce la lista di tutti gli eventi esistenti."""
     return session.exec(select(Event)).all()
+
+@router.get("/{id}")
+def get_event(
+        session: SessionDep,
+        id: Annotated[int, Path(description="The ID of the event to get")]
+) -> EventPublic:
+    """Restituisce l'evento con l'id indicato, o 404 se non esiste."""
+    event = session.get(Event, id)
+    if event:
+        return event
+    else:
+        raise HTTPException(status_code=404, detail="Event not found")
