4c42ab2afdf0622c8edfaff38c353a53eac77a3c
Feagly <paolowiiu@gmail.com>
2026-06-22 17:22:49 +0200

    feat: implementa endpoint PUT /events/{id}

--- app/routers/events.py
+++ app/routers/events.py
@@ -30,3 +30,21 @@ def add_event(event: EventCreate, session: SessionDep):
     session.add(Event.model_validate(event))
     session.commit()
     return "Event successfully added"
+
+@router.put("/{id}")
+def update_event(
+        session: SessionDep,
+        id: Annotated[int, Path(description="The ID of the event to update")],
+        new_event: EventCreate
+):
+    """Aggiorna l'evento con l'id indicato con i nuovi dati forniti."""
+    event = session.get(Event, id)
+    if not event:
+        raise HTTPException(status_code=404, detail="Event not found")
+    event.title = new_event.title
+    event.description = new_event.description
+    event.date = new_event.date
+    event.location = new_event.location
+    session.add(event)
+    session.commit()
+    return "Event successfully updated"
