a48e8bba5c801bdd5d9bc96be16f0131e9cf6b27
Feagly <paolowiiu@gmail.com>
2026-06-23 11:04:22 +0200

    feat: aggiunta API POST /users con controllo username duplicato

--- app/routers/users.py
+++ app/routers/users.py
@@ -12,3 +12,13 @@ router = APIRouter(prefix="/users")
 def get_all_users(session: SessionDep) -> list[UserPublic]:
     """Restituisce la lista di tutti gli utenti esistenti."""
     return session.exec(select(User)).all()
+
+@router.post("/")
+def add_user(new_user: UserCreate, session: SessionDep):
+    """Crea un nuovo utente, errore se lo username esiste già."""
+    user = session.get(User, new_user.username)
+    if user:
+        raise HTTPException(status_code=409, detail="Username already exists")
+    session.add(User.model_validate(new_user))
+    session.commit()
+    return "User successfully added"
